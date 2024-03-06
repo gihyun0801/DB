7a21276ce5e79c2fe649602068099e1695fe6795
GH <Matchlessssss>
2024-03-06 17:10:32 +0900

    dasdsa

---
+++ 05_DML(INSERT,UPDATE,DELETE).sql
@@ -0,0 +1,84 @@
+-- *** DML(Date Manipulation Language) : 데이터 조작 언어
+
+-- 테이블에 있는 값을 삽입하거나(INSERT)
+-- 수정하거나 (UPDATE)
+-- 삭제하거나 (DELETE) 하는 구문
+
+-- 주의 : 혼자서 COMMIT, ROLLBACK 하지 말것! (하면 다른 결과 값이 나올수도 있음)
+
+
+
+-- 테스트용 테이블 생성
+CREATE TABLE EMPLOYEE2 AS SELECT * FROM EMPLOYEE;
+CREATE TABLE DEPARTMENT2 AS SELECT * FROM DEPARTMENT;
+
+SELECT * FROM EMPLOYEE2;
+SELECT * FROM DEPARTMENT2;
+
+
+--------------------------------------------------------------------------------
+
+-- 1. INSERT 
+
+-- 테이블에 새로운 행을 추가하는 구문
+
+-- 1) INSERT INTO 테이블명 VALUES(데이터, 데이터, 데이터,.....)
+-- 테이블에 있는 모든 컬럼에 대한 값을 INSERT 할 때 사용
+-- INSERT 하고자 하는 컬럼이 모든 컬럼인 겨우 컬럼명 생략 가능.
+-- 단, 컬럼의 순서를 지켜서 VALUES에 값을 기입해야함.
+
+INSERT INTO EMPLOYEE2 
+VALUES('900','성기현','000801-3032811','gihyun0801@naver.com',01066432026,'D1'
+,'J7', 'S3', 4300000, 0.2, '200', SYSDATE, NULL, 'N');
+
+
+SELECT * FROM EMPLOYEE2
+WHERE EMP_ID = '900';
+
+ROLLBACK;
+
+--ROLLBACK COMMIT 되지 않은 데이터를 없애버림
+
+
+-- 2) INSERT INTO 테이블명(컬럼명, 컬럼명, 컬럼명...) 
+     -- VALUES(데이터, 데이터, 데이터...)
+-- 테이블에 내가 선택한 컬럼에 대한 값만 INSERT 할 때 사용
+-- 선택 안된 컬럼은 NULL 이 들어감 (DEFAULT 존재 시 DEFAULT 값이 들어감)
+
+INSERT INTO EMPLOYEE2(EMP_ID, EMP_NAME, EMP_NO, EMAIL, PHONE,
+DEPT_CODE, JOB_CODE, SAL_LEVEL, SALARY)
+VALUES('900','성기현','000801-3032811','gihyun0801@naver.com',01066432026,
+'D1', 'J7','S3',4300000);
+
+COMMIT;
+
+--COMMIT 을 해버렷기 때문에 ROLLBACK 해도 초기화가 안됌
+-- 왜 여기선 DEFALUT 값이 없냐 이유는 EMPLOYEE 를 복사하엿지만 그 조건들까진 복사를 안했기
+--떄문에 디폴트값엔 아무것도 없는 상태이다. 그래서 NULL 이 뜨는 것이다
+
+
+---------------------------------------------------------------------------
+
+-- (참고) INSERT 시 VALUES 대신 서브쿼리 사용 가능
+CREATE TABLE EMP_01(
+  EMP_ID NUMBER,
+  EMP_NAME VARCHAR2(30),
+  DEPT_TITLE VARCHAR2(20)
+);
+
+SELECT * FROM EMP_01;
+
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE2
+LEFT JOIN DEPARTMENT2 ON(DEPT_CODE = DEPT_ID);
+
+-- 서브쿼리(SELECT) 결과를 EMP_01 테이블에 INSERT
+--> SELECT 조회 결과의 데이터 타입, 컬럼 개수가
+-- INSERT 하려는 테이블의 컬럼과 일치해야함
+INSERT INTO EMP_01(
+SELECT EMP_ID, EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE2
+LEFT JOIN DEPARTMENT2 ON(DEPT_CODE = DEPT_ID));
+
+------------------------------------------------------------------------------------------
+
---
+++ 06_TCL(ROLLBACK, COMMIT).sql
@@ -0,0 +1,80 @@
+-- TCL (Transaction Control Language) : 트랜잭션 제어 언어
+-- COMMIT, ROLLBACK, SAVEPOINT
+
+-- COMMIT : 트랜잭션 종료 후 저장
+-- ROLLBACK : 트랜잭션 취소
+-- SAVEPOINT : 임시 저장
+
+-- DML : 데이터 조작 언어 INSERT, UPDATE, DELETE
+--> 트랜잭션은 DML과 관련되어 있음
+
+/* 트랜잭션 이란?
+ * - 데이터베이스의 논리적 연산 단위
+ * - 데이터 변경 사항을 묶어서 하나의 트랜잭션에 담아 처리함.
+ * - 트랜잭션의 대상이 되는 데이터 변경 사항 : INSERT, UPDATE, DELETE, MERGE
+ * 
+ * INSERT 수행 ---------------------------------------------> DB 반영 (X)
+ * 
+ * INSERT 를 하면 트랜잭션이란 바구니에 담기는 거고 -- COMMIT 을 하면 이제 바구니에 담겨져 있는게
+ * 실제 DB 에 반영되는거다
+ * 
+ * 
+ * INSERT 10번 수행 --> 1개 트랜잭션에 10개 수행한 구문에 추가되어있다 -> ROLLABACK --> DB반영 X
+ * 
+ * 1) COMMIT : 메모리 버퍼 (트랜잭션)에 임시 저장된 데이터 변경 사항을 DB에 반영
+ * 
+ * 2) ROLLBACK : 메모리 버퍼(트랜잭션)에 임시 저장된 데이터 변경 사항을 삭제하고
+ *  						마지막 COMMIT 상태로 돌아감(DB에 변경 내용 반영X)
+ * 
+ * 3) SAVEPOINT : 메모리 버퍼(트랜잭션)에 저장 지점을 정의하여
+ *  						 ROLLBACK 수행 시 전체 작업을 삭제하는 것이 아닌
+ * 								저장 지점까지만 일부 ROLLBACK
+ * 
+ * [SAVEPOINT 사용법]
+ * 
+ * SAVEPOINT  "포인트명1"; 
+ * 
+ * 
+ * SAVEPOINT  "포인트명2"; 
+ * 
+ * 
+ * ROLLBACK TO "포인트명1"; -- 포인트 1 로 돌아감
+ * 
+ *  SAVEPOINT 지정 및 호출 시 이름에 ""(쌍따옴표) 붙여야함
+ * 
+ * */
+
+-- 새로운 데이터 INSERT
+SELECT * FROM DEPARTMENT2;
+
+INSERT INTO DEPARTMENT2 VALUES('T1', '개발 1팀', 'L2');
+INSERT INTO DEPARTMENT2 VALUES('T2', '개발 2팀', 'L2');
+INSERT INTO DEPARTMENT2 VALUES('T3', '개발 3팀', 'L2');
+
+
+SELECT * FROM DEPARTMENT2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
